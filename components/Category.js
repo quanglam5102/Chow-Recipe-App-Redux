c637b50488730ac45465d2fd914ef8d8815cead0
Leyla <Leyla.osmani@outlook.com>
2022-12-13 19:32:40 -0500

    Finished the Home Component and included image files.

--- components/Category.js
+++ components/Category.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import { View, StyleSheet, ScrollView, Text, TouchableOpacity } from 'react-native';
 import { Appbar, Searchbar, Paragraph, Card } from 'react-native-paper';
 
-export default function Category({ navigation }) {
+export default function Category() {
   const [meals, setMeals] = useState([]);
   const [search, setSearch] = useState('');
   const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
@@ -18,29 +18,21 @@ export default function Category({ navigation }) {
   }, []);
 
   const onChangeSearch = (query) => setSearch(query);
-  // console.log(meals)
+  console.log(meals)
   return (
     <View>
-      <TouchableOpacity
-        style={styles.reviewbutton}
-        onPress={() => navigation.navigate('RandomMeal')}>
-        <Text style={{fontSize: 20, left: 300}}>
-            Random Meal
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.reviewbutton}
-        onPress={() => navigation.navigate('SearchByArea')}>
-        <Text style={{fontSize: 20, left: 300}}>
-            Search By Area
-        </Text>
-      </TouchableOpacity>
       <Appbar style={{ backgroundColor: '#6B1818'}}>
       {/* button for navigation, with the styling 
       to place the button in the top right corner. 
       Left the navigation empty for whatever the next page might be
       */}
-      
+      <TouchableOpacity
+        style={styles.reviewbutton}
+        onPress={() => navigation.navigate('')}>
+        <Text style={{fontSize: 20, left: 300}}>
+            Next
+          </Text>
+      </TouchableOpacity>
         <Appbar.Content title="Chow!" textAlign="center" />
         <Appbar.Header
           style={{ backgroundColor: '#6B1818'}}></Appbar.Header>
--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,107 @@
+import { useState, useRef } from 'react';
 import * as React from 'react';
-import { Text, View, StyleSheet, Image, Button } from 'react-native';
+import StarRating from 'react-native-star-rating-widget';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  StatusBar,
+  ScrollView,
+  Modal,
+  Button,
+  Alert
+} from 'react-native';
+import { Appbar} from 'react-native-paper';
 
-export default function Home({navigation, route}) {
-    return (
-      <View>
-        <Text>This is the HomePage</Text>
-        <Button title="Go to Recipes Page" onPress={() => {
-        navigation.navigate("Recipes")
-      }} />
-      </View>
-    );
+function Seperator() {
+  return <View style={styles.seperator}></View>;
 }
+function HomeScreen({ navigation }) {
+    const [showModal, setShowModal] = useState(false);
+    var button1 = [
+    {
+      text: 'Yes',
+      onPress: () => {
+        setShowModal(!showModal);
+      },
+    },
+    { text: 'No', onPress: () => {} },
+  ]; 
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('./logo.png')} />
+         <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          Alert.alert(
+            'Attention',
+            'Are you sure you want to continue?',
+            button1
+          );
+        }}>
+        <Text style={styles.buttonText}>About Us</Text>
+      </TouchableOpacity>
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={showModal}
+        onRequestClose={() => {}}>
+        <View style={styles.container}>
+        <Image style={styles.logo} source={require('./logo.png')} />
+      <Text style={styles.modalText}>
+            Welcome to CHOW food recipes where you’ll be able to learn how to make amazing dishes with the help of our many features. You’ll be able to browse through dozens of recipes with the ingredients to go with it. You’ll also be able to search for specific meals you want to to enjoy from all over the world all here. We love to make cooking EASY!
+          </Text>
+          <Button title="Close" onPress={() => setShowModal(!showModal)} />
+        </View>
+      </Modal>
+         <Seperator/>
+         <ScrollView>
+      <Image style={styles.logo} source={require('./pasta.jpeg')} />
+       <Image style={styles.logo} source={require('./sushi.jpeg')} />
+       <Image style={styles.logo} source={require('./noodles.jpeg')} />
+       <Image style={styles.logo} source={require('./rice.jpeg')} />
+       <Image style={styles.logo} source={require('./sandwitch.jpeg')} />
+       </ScrollView>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FCFBE0',
+    padding: 20,
+    flex: 1,
+    alignItems: 'center', 
+    justifyContent: 'center',
+  },
+  logo: {
+    height: 350,
+    width: 300,
+    
+  },
+  button: {
+    display: 'flex',
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '70%',
+    backgroundColor: '#6E8898',
+    shadowColor: '#6E8898',
+    shadowOpacity: 0.9,
+    shadowOffset: {
+      height: 10,
+      width: 10,
+    },
+    shadowRadius: 30,
+  },
+  buttonText:{
+    fontSize:20
+  },
+  modalText:{
+    fontSize: 20
+  },
+  seperator: { margin: 10 },
+});
+export default HomeScreen;
